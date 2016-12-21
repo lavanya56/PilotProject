fdfb96607898cbadbb4b346304b32cae334ff378
lavanya56 <pujarilavanya4@gmail.com>
2016-12-21 19:06:23 +0530

    Update TurnipWebConstants.java

--- src/com/miracle/service/restclient/TurnipWebConstants.java
+++ src/com/miracle/service/restclient/TurnipWebConstants.java
@@ -3,7 +3,7 @@ package com.miracle.service.restclient;
 public class TurnipWebConstants {
 	public static final String METHOD_NAME = "GET";
 	//172.17.16.151
-	public static final String VENDOR_CUSTOMER_LIST_URL = "172.17.13.16:7080/req/";
+	public static final String VENDOR_CUSTOMER_LIST_URL = "172.17.12.81:7080/req/";
 	
 	
 
